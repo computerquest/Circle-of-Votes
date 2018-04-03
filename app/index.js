4913a8584896c4dc77548073154f00bd7112a957
jaredstigter@gmail.com <jaredstigter@gmail.com>
2018-04-03 02:01:23 -0400

    trimed the inputs changed the port to 9000

--- app/index.js
+++ app/index.js
@@ -1,26 +1,19 @@
 const express = require('express')
-var Congress = require('propublica-congress-node');   //maximum of 5000 calls per day
 var OpenSecretsClient = require('opensecrets-client', 'json'); //maximum of 200 calls per day
-var util = require("util")
-var request = require('request');
 var mergeJSON = require("merge-json");
 var fs = require('fs');
-var net = require('net'),
-JsonSocket = require('json-socket');
 var mustacheExpress = require('mustache-express');
+ppc = require('propublica-congress').create('CfNPRL9q6wPC8iEHEG4PhZk9xiQbcWSTvVFjqItF');
 
 const app = express()
 app.set('view engine', 'mustache')
 app.engine('mustache', mustacheExpress());
 app.set('views', __dirname + '/public'); // you can change '/views' to '/public',
-ppc = require('propublica-congress').create('CfNPRL9q6wPC8iEHEG4PhZk9xiQbcWSTvVFjqItF');
 var clientO = new OpenSecretsClient('8fad4c535bd7763204689b57c70137fd'); //the json values are returned as a string
 
 var opKeys = ['8fad4c535bd7763204689b57c70137fd', 'd54dbd5a4f572862c2609aab9487a365', 'f8cea77db428d13c088ac8afff35e519', 'be091217e1dd3b340e2511e38699efa7', 'db37558aa3f970cadfb8345c26d1dde6','5928e99d96fac2a30906a126a293714d', '1ad00b500ae4d8a0a3333c7e1689eebb', 'c29969ede23d4f4871205c97548a8290', '2d87571b3707af874843d8e9f3391666']
 
-app.use(express.static('public'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(9000, () => console.log('Example app listening on port 3000!'))
 
 app.get('/search/:query', function(req, res) {
     //res.setHeader('Content-Type', 'text/html')
